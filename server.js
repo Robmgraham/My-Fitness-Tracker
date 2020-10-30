9ce3d8e1121267e7ff5fec61715b28c1401c4a18
rancid <robmgraham13@yahoo.com>
2020-10-29 18:39:37 -0700

    added files

---
+++ server.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const express = require('express');
+const logger = require("morgan");
+const path = require("path");
+const app = express();
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+const db = require("./models")
+
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/stats.html"));
+});
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/exercise.html"));
+});
+
+app.get("/api/workouts", (req, res) => {
+    db.Workout.find().then(function(data) {
+        console.log(data)
+        res.json(data)
+    })
+})
+
+app.get("/api/workouts/range", (req, res) => {
+    db.Workout.find().then(function(data) {
+        console.log(data)
+        res.json(data)
+    })
+})
+
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout")
+
+app.listen(3000, () => {
+    console.log("App running on port 3000!");
+});
